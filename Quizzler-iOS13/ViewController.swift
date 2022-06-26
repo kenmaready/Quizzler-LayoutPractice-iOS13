49faff718436fe34379099f7410fb330a68282ba
kenmaready <ken@maready.com>
2022-06-26 09:16:18 -0400

    Added launch screen, connected UI elements to script, display question on launch

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionTextView: UILabel!
+    @IBOutlet weak var progressBarView: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionTextView.text = "Four + Two is equal to Six"
     }
 
-
+    @IBAction func userAnswerSubmitted(_ sender: UIButton) {
+    }
+    
 }
 
