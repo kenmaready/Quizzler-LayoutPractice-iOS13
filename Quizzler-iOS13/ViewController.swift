0bfed7455532368b95b767fec722e6288cfe4b45
kenmaready <ken@maready.com>
2022-06-26 10:18:34 -0400

    Updated quiz to use Swift struct datatype for qs and as

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+struct QuizQuestion {
+    let q: String
+    let a: Bool
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var questionTextView: UILabel!
@@ -16,18 +21,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let questions = [
-            (q:"A slug's blood is green.", a:true),
-            (q:"Approximately one quarter of human bones are in the feet.", a:true),
-            (q:"The total surface area of two human lungs is approximately 70 square metres.", a:true),
-             (q:"In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a:true),
-             (q:"In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a:false),
-             (q:"It is illegal to pee in the Ocean in Portugal.", a:true),
-             (q:"You can lead a cow down stairs but not up stairs.", a:false),
-             (q:"Google was originally called 'Backrub'.", a:true),
-             (q:"Buzz Aldrin's mother's maiden name was 'Moon'.", a:true),
-             (q:"The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a:false),
-             (q:"No piece of square dry paper can be folded in half more than 7 times.", a:false),
-             (q:"Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a:true),
+        QuizQuestion(q:"A slug's blood is green.", a:true),
+        QuizQuestion(q:"Approximately one quarter of human bones are in the feet.", a:true),
+        QuizQuestion(q:"The total surface area of two human lungs is approximately 70 square metres.", a:true),
+        QuizQuestion(q:"In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a:true),
+        QuizQuestion(q:"In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a:false),
+        QuizQuestion(q:"It is illegal to pee in the Ocean in Portugal.", a:true),
+        QuizQuestion(q:"You can lead a cow down stairs but not up stairs.", a:false),
+        QuizQuestion(q:"Google was originally called 'Backrub'.", a:true),
+        QuizQuestion(q:"Buzz Aldrin's mother's maiden name was 'Moon'.", a:true),
+        QuizQuestion(q:"The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a:false),
+        QuizQuestion(q:"No piece of square dry paper can be folded in half more than 7 times.", a:false),
+        QuizQuestion(q:"Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a:true),
     ]
     var currentQuizItem = 0
     var score = 0
